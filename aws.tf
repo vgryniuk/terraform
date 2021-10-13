de922a1eb1b3bb4c3a5d708335c2740f599ed657
vgryniuk <grynjuk@gmail.com>
2021-10-13 17:12:42 +0300

    first commit

---
+++ aws.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+    access_key = "AKIAXBCSZEBY6K5L7ARP"
+    secret_key = "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"
+    region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "vg_tf_lessons" {
+  bucket = "vg-test-bucket-tf-001"
+  acl    = "private"
+}
+
+resource "aws_db_instance" "vg_tf_lessons" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "vgdb"
+  username             = "admin"
+  password             = "admin-admin"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "vgtestinstance"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "VG Test"
+  }
+}
